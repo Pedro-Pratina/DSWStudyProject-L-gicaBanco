b5a7380471fd75e35427ed387ba3f1db40193e67
Pedro Henrique Reis Lima <160922340+Pedro-Pratina@users.noreply.github.com>
2024-11-26 21:41:09 -0300

    feat: Funções finalizadas! Creio eu que está certo!

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const numeroDaConta = input(`Digite um número de 5 digitos para sua conta: `)
 let saldo = 0
 let extrato = []
 
-const detalheDaConta = `\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo}\n`
-
-console.log(`\tConsta criada com sucesso!\n\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo}\n`)
+console.log(`\tConta criada com sucesso!\n\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo.toFixed(2)}\n`)
 
 let resposta = 0
 
@@ -22,16 +20,35 @@ while(resposta !== 5){
     resposta = Number(input(`Digite a opção: `))
 
     if(resposta === 1){
-        console.log(detalheDaConta)
+        console.log(`Opção escolhida: 1 - Saldo`)
+        console.log(`\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo.toFixed(2)}\n`)
     } else if(resposta === 2){
-        console.log(detalheDaConta)
+        if(extrato.length > 0){
+            for(let i = 0; extrato.length > i; i++){
+                console.log(extrato[i])
+            }
+        } else {
+            console.log(`Nenhuma operação foi feita na conta!`)
+        }
     } else if(resposta === 3){
-        console.log(detalheDaConta)
+        let valorSaque = Number(input(`Digite o valor que deseja sacar: `))
+        
+        if(valorSaque <= saldo){
+            saldo -= valorSaque
+            console.log(`\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo.toFixed(2)}\n`)
+            extrato.push(`Operação: Saque, Valor: R$${valorSaque.toFixed(2)}, Saldo após: R$${saldo.toFixed(2)}`)
+        } else {
+            console.log(`Saldo insuficiente para efetuar o saque!`)
+        }
     } else if(resposta === 4){
-        console.log(detalheDaConta)
+        let valorDeposito = Number(input(`Digite o valor que deseja depositar: `))
+
+        saldo += valorDeposito
+        extrato.push(`Operação: Depósito, Valor: R$:${valorDeposito.toFixed(2)}, Saldo após: R$${saldo.toFixed(2)}`)
+        console.log(`\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo.toFixed(2)}\n`)
     } else if(resposta === 5){
         console.log(`Você escolheu sair. Obrigado por escolher o Banco Pratina!\n`)
-        console.log(detalheDaConta)
+        console.log(`\n\tDetalhes da conta:\n\t\tNúmero da conta: ${numeroDaConta}\n\t\tNome do titular: ${nomeDoCliente}\n\t\tSaldo atual: ${saldo.toFixed(2)}\n`)
     } else {
         console.log(`\nOpção invalida!\n`)
     }
